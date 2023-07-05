a68c15d0ab613254509b6ce7a8ffcbcfa8aa71f5
Michael Okolo <michaelokolo62@yahoo.com>
2023-07-05 17:34:22 -0400

    using public for css js and images

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,29 +1,17 @@
-const http = require('http')
+const express = require('express')
+const path =require('path')
+const app = express()
 
-// check for MIME type - media types in MDN
-// check for the status code - MDN
+app.use(express.static('./public'))
 
-const server = http.createServer((req,res)=>{
-    // console.log(req.method)
-    const url = req.url;
-    //home page
-    if(url === '/'){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1>home page</h1>')
-        res.end()
-    // about page
-    }else if(url === '/about'){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1>about page</h1>')
-        res.end()
-    }
-    // 404
-    else{
-        res.writeHead(404,{'content-type':'text/html'})
-        res.write('<h1>page not found</h1>')
-        res.end()
-    }
-    
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+}) 
+
+app.all('*',(req,res)=>{
+    res.status(404).send(`resource not found`)
 })
 
-server.listen(5000)
+app.listen(5000, ()=>{
+    console.log(`server is listening on port 5000...`)
+})
